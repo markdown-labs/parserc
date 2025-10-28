6d303328fadb6dd79e747a587e9b081f5a27d475
yayanyang <yayanyang@gmail.com>
2025-10-28 11:11:23 +0800

    update

--- crates/parserc/src/lib.rs
+++ crates/parserc/src/lib.rs
@@ -1,19 +1,2 @@
 //! Parse combinator framework for `rust` language.
 #![cfg_attr(docsrs, feature(doc_cfg))]
-
-mod errors;
-pub use errors::*;
-mod input;
-pub use input::*;
-mod lexer;
-pub use lexer::*;
-mod parser;
-pub use parser::*;
-
-#[cfg(feature = "lang")]
-#[cfg_attr(docsrs, doc(cfg(feature = "lang")))]
-pub mod lang;
-
-#[cfg(feature = "syntax")]
-#[cfg_attr(docsrs, doc(cfg(feature = "syntax")))]
-pub mod syntax;
